fdc980bb80603229ba25659632fda58fe3ba3310
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2020-09-06 10:22:07 +0300

    835. Image Overlap

---
+++ 835-image-overlap/main.py
@@ -0,0 +1,37 @@
+import collections
+import logging
+from typing import List
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class Solution:
+    def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
+        n = len(A)
+        pts1 = [
+            (i,j)
+            for i in range(n)
+            for j in range(n)
+            if A[i][j] == 1
+        ]
+        pts2 = [
+            (i,j)
+            for i in range(n)
+            for j in range(n)
+            if B[i][j] == 1
+        ]
+        counter = collections.Counter(
+            (x1 - x2, y1 - y2)
+            for x1, y1 in pts1
+            for x2, y2 in pts2
+        )
+        return max(counter.values() or [0])
+
+
+def main():
+    assert Solution().largestOverlap(A=[[1,1,0], [0,1,0], [0,1,0]], B=[[0,0,0], [0,1,1], [0,0,1]]) == 3
+
+
+if __name__ == '__main__':
+    main()
